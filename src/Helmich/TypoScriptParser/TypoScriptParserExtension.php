3205e907779f77cf4cefeb443af63b0f2090b234
Martin Helmich <m.helmich@mittwald.de>
2014-06-18 21:29:16 +0200

    Configure extension for dependency injection.

---
+++ src/Helmich/TypoScriptParser/TypoScriptParserExtension.php
@@ -0,0 +1,75 @@
+<?php
+namespace Helmich\TypoScriptParser;
+
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+
+class TypoScriptParserExtension implements ExtensionInterface
+{
+
+
+
+    /**
+     * Loads a specific configuration.
+     *
+     * @param array            $config    An array of configuration values
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     *
+     * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     *
+     * @api
+     */
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../../config'));
+        $loader->load('services.yml');
+    }
+
+
+
+    /**
+     * Returns the namespace to be used for this extension (XML namespace).
+     *
+     * @return string The XML namespace
+     *
+     * @api
+     */
+    public function getNamespace()
+    {
+        // TODO: Implement getNamespace() method.
+    }
+
+
+
+    /**
+     * Returns the base path for the XSD files.
+     *
+     * @return string The XSD base path
+     *
+     * @api
+     */
+    public function getXsdValidationBasePath()
+    {
+        // TODO: Implement getXsdValidationBasePath() method.
+    }
+
+
+
+    /**
+     * Returns the recommended alias to use in XML.
+     *
+     * This alias is also the mandatory prefix to use when using YAML.
+     *
+     * @return string The alias
+     *
+     * @api
+     */
+    public function getAlias()
+    {
+        return 'typoscript_parser';
+    }
+}
