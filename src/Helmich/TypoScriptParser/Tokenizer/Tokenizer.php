5725728f1add061dcbf8f337a64dddc5666e1996
Martin Helmich <m.helmich@mittwald.de>
2016-03-20 13:31:03 +0100

    Adjust tokenizer

--- src/Helmich/TypoScriptParser/Tokenizer/Tokenizer.php
+++ src/Helmich/TypoScriptParser/Tokenizer/Tokenizer.php
@@ -13,8 +13,8 @@ class Tokenizer implements TokenizerInterface
     const TOKEN_CONDITION_END = ',^\[(global|end)\],i';
 
     const TOKEN_OBJECT_NAME = ',^(CASE|CLEARGIF|COA(?:_INT)?|COBJ_ARRAY|COLUMNS|CTABLE|EDITPANEL|FILES?|FLUIDTEMPLATE|FORM|HMENU|HRULER|TEXT|IMAGE|IMG_RESOURCE|IMGTEXT|LOAD_REGISTER|MEDIA|MULTIMEDIA|OTABLE|QTOBJECT|RECORDS|RESTORE_REGISTER|SEARCHRESULT|SVG|SWFOBJECT|TEMPLATE|USER(?:_INT)?|GIFBUILDER|[GT]MENU(?:_LAYERS)?|(?:G|T|JS|IMG)MENUITEM)$,';
-    const TOKEN_OBJECT_ACCESSOR = ',^([a-zA-Z0-9_\-]+(?:\.[a-zA-Z0-9_\-]+)*)$,';
-    const TOKEN_OBJECT_REFERENCE = ',^\.?([a-zA-Z0-9_\-]+(?:\.[a-zA-Z0-9_\-]+)*)$,';
+    const TOKEN_OBJECT_ACCESSOR = ',^([a-zA-Z0-9_\-\\\\]+(?:\.[a-zA-Z0-9_\-\\\\]+)*)$,';
+    const TOKEN_OBJECT_REFERENCE = ',^\.?([a-zA-Z0-9_\-\\\\]+(?:\.[a-zA-Z0-9_\-\\\\]+)*)$,';
 
     const TOKEN_NESTING_START = ',^\{$,';
     const TOKEN_NESTING_END = ',^\}$,';
@@ -27,7 +27,7 @@ class Tokenizer implements TokenizerInterface
         \)
     $,x';
     const TOKEN_OPERATOR_LINE = ',^
-        ([a-zA-Z0-9_\-]+(?:\.[a-zA-Z0-9_\-]+)*)   # Left value (object accessor)
+        ([a-zA-Z0-9_\-\\\\]+(?:\.[a-zA-Z0-9_\-\\\\]+)*)   # Left value (object accessor)
         (\s*)                                     # Whitespace
         (=|:=|<=|<|>|\{|\()                       # Operator
         (\s*)                                     # More whitespace
