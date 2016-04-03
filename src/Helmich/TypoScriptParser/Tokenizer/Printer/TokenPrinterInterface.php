cafacb882e21728eb3af6b14a6102b2bfb3dbb51
Martin Helmich <m.helmich@mittwald.de>
2016-04-03 16:52:19 +0200

    Test commit

--- src/Helmich/TypoScriptParser/Tokenizer/Printer/TokenPrinterInterface.php
+++ src/Helmich/TypoScriptParser/Tokenizer/Printer/TokenPrinterInterface.php
@@ -3,6 +3,12 @@ namespace Helmich\TypoScriptParser\Tokenizer\Printer;
 
 use Helmich\TypoScriptParser\Tokenizer\TokenInterface;
 
+/**
+ * Interface definition for a class that prints token streams
+ *
+ * @package    Helmich\TypoScriptParser
+ * @subpackage Tokenizer\Printer
+ */
 interface TokenPrinterInterface
 {
     /**
