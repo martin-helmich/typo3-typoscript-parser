92104f40cef1db61e6e1f4e6018e2278e6eeb161
Martin Helmich <kontakt@martin-helmich.de>
2017-04-09 17:42:32 +0200

    Add interface definition for preprocessors

---
+++ src/Helmich/TypoScriptParser/Tokenizer/Preprocessing/Preprocessor.php
@@ -0,0 +1,22 @@
+<?php
+namespace Helmich\TypoScriptParser\Tokenizer\Preprocessing;
+
+
+/**
+ * Interface definitions for tokenizer preprocessors.
+ *
+ * Preprocessors can change the TypoScript input source code before it
+ * is passed to the actual tokenizer.
+ *
+ * @package Helmich\TypoScriptParser\Tokenizer\Preprocessing
+ */
+interface Preprocessor
+{
+
+    /**
+     * @param string $contents Un-processed Typoscript contents
+     * @return string Processed TypoScript contents
+     */
+    public function preprocess($contents);
+
+}
