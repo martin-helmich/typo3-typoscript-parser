7e514a6bad1276a8b73bafd5046f472d1274d961
Martin Helmich <kontakt@martin-helmich.de>
2017-12-11 20:03:55 +0100

    Add failing test case for #27

---
+++ tests/functional/Parser/Fixtures/Assignments/condition_elseif.php
@@ -0,0 +1,26 @@
+<?php
+return [
+    new \Helmich\TypoScriptParser\Parser\AST\ConditionalStatement(
+        '[globalVar = GP:foo=1]', [
+        new \Helmich\TypoScriptParser\Parser\AST\Operator\Assignment(
+            new \Helmich\TypoScriptParser\Parser\AST\ObjectPath('foo', 'foo'),
+            new \Helmich\TypoScriptParser\Parser\AST\Scalar('bar'),
+            2
+        ),
+    ], [], 1),
+    new \Helmich\TypoScriptParser\Parser\AST\ConditionalStatement(
+        '[globalVar = GP:foo=2]', [
+        new \Helmich\TypoScriptParser\Parser\AST\Operator\Assignment(
+            new \Helmich\TypoScriptParser\Parser\AST\ObjectPath('foo', 'foo'),
+            new \Helmich\TypoScriptParser\Parser\AST\Scalar('bar2'),
+            4
+        ),
+    ], [
+            new \Helmich\TypoScriptParser\Parser\AST\Operator\Assignment(
+                new \Helmich\TypoScriptParser\Parser\AST\ObjectPath('foo', 'foo'),
+                new \Helmich\TypoScriptParser\Parser\AST\Scalar('baz'),
+                6
+            ),
+        ], 3
+    ),
+];
