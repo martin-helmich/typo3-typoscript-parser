207f48bbb5746382115f4b3e38e26903471fff8a
Martin Helmich <kontakt@martin-helmich.de>
2018-02-19 18:52:31 +0100

    Add tokenization rules for new syntax

--- tests/unit/Tokenizer/TokenizerTest.php
+++ tests/unit/Tokenizer/TokenizerTest.php
@@ -1,9 +1,9 @@
 <?php
+
 namespace Helmich\TypoScriptParser\Tests\Unit\Tokenizer;
 
 use Helmich\TypoScriptParser\Tokenizer\Token;
 use Helmich\TypoScriptParser\Tokenizer\Tokenizer;
-use Helmich\TypoScriptParser\Tokenizer\TokenizerException;
 use Helmich\TypoScriptParser\Tokenizer\TokenizerInterface;
 use VirtualFileSystem\FileSystem;
 
@@ -33,7 +33,7 @@ class TokenizerTest extends \PHPUnit_Framework_TestCase
             new Token(Token::TYPE_WHITESPACE, " ", 1, 4),
             new Token(Token::TYPE_OPERATOR_ASSIGNMENT, "=", 1, 5),
             new Token(Token::TYPE_WHITESPACE, " ", 1, 6),
-            new Token(Token::TYPE_RIGHTVALUE, "bar", 1, 7)
+            new Token(Token::TYPE_RIGHTVALUE, "bar", 1, 7),
         ]];
 
         yield ["foo.bar = baz", [
@@ -41,7 +41,7 @@ class TokenizerTest extends \PHPUnit_Framework_TestCase
             new Token(Token::TYPE_WHITESPACE, " ", 1, 8),
             new Token(Token::TYPE_OPERATOR_ASSIGNMENT, "=", 1, 9),
             new Token(Token::TYPE_WHITESPACE, " ", 1, 10),
-            new Token(Token::TYPE_RIGHTVALUE, "baz", 1, 11)
+            new Token(Token::TYPE_RIGHTVALUE, "baz", 1, 11),
         ]];
 
         yield ["foo.bar.baz = baz", [
@@ -49,7 +49,7 @@ class TokenizerTest extends \PHPUnit_Framework_TestCase
             new Token(Token::TYPE_WHITESPACE, " ", 1, 12),
             new Token(Token::TYPE_OPERATOR_ASSIGNMENT, "=", 1, 13),
             new Token(Token::TYPE_WHITESPACE, " ", 1, 14),
-            new Token(Token::TYPE_RIGHTVALUE, "baz", 1, 15)
+            new Token(Token::TYPE_RIGHTVALUE, "baz", 1, 15),
         ]];
 
         yield ["foo =< bar", [
@@ -57,7 +57,7 @@ class TokenizerTest extends \PHPUnit_Framework_TestCase
             new Token(Token::TYPE_WHITESPACE, " ", 1, 4),
             new Token(Token::TYPE_OPERATOR_REFERENCE, "=<", 1, 5),
             new Token(Token::TYPE_WHITESPACE, " ", 1, 7),
-            new Token(Token::TYPE_OBJECT_IDENTIFIER, "bar", 1, 8)
+            new Token(Token::TYPE_OBJECT_IDENTIFIER, "bar", 1, 8),
         ]];
 
         yield ["foo > # Something", [
@@ -65,7 +65,25 @@ class TokenizerTest extends \PHPUnit_Framework_TestCase
             new Token(Token::TYPE_WHITESPACE, " ", 1, 4),
             new Token(Token::TYPE_OPERATOR_DELETE, ">", 1, 5),
             new Token(Token::TYPE_WHITESPACE, " ", 1, 6),
-            new Token(Token::TYPE_COMMENT_ONELINE, "# Something", 1, 7)
+            new Token(Token::TYPE_COMMENT_ONELINE, "# Something", 1, 7),
+        ]];
+
+        yield ["<INCLUDE_TYPOSCRIPT: source=\"FILE:EXT:foo/Configuration/TypoScript/setup.typoscript\">", [
+            new Token(Token::TYPE_INCLUDE, "<INCLUDE_TYPOSCRIPT: source=\"FILE:EXT:foo/Configuration/TypoScript/setup.typoscript\">", 1, 1, [
+                0          => "<INCLUDE_TYPOSCRIPT: source=\"FILE:EXT:foo/Configuration/TypoScript/setup.typoscript\">",
+                'type'     => 'FILE',
+                1          => 'FILE',
+                'filename' => 'EXT:foo/Configuration/TypoScript/setup.typoscript',
+                2          => 'EXT:foo/Configuration/TypoScript/setup.typoscript',
+            ]),
+        ]];
+
+        yield ["@import 'EXT:foo/Configuration/TypoScript/setup.typoscript'", [
+            new Token(Token::TYPE_INCLUDE_NEW, "@import 'EXT:foo/Configuration/TypoScript/setup.typoscript'", 1, 1, [
+                0          => "@import 'EXT:foo/Configuration/TypoScript/setup.typoscript'",
+                'filename' => 'EXT:foo/Configuration/TypoScript/setup.typoscript',
+                1          => 'EXT:foo/Configuration/TypoScript/setup.typoscript',
+            ]),
         ]];
     }
 
