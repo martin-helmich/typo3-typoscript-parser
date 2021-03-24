5ebfb34a3c566f9ce5da3163fe93d83d73cd0401
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-03-24 21:58:49 +0100

    [WIP] Failing test for multiple conditions in one file
    Relates: #52

---
+++ tests/functional/Parser/Fixtures/Assignments/multiple_conditions.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+return [
+    new \Helmich\TypoScriptParser\Parser\AST\ConditionalStatement(
+        '[globalVar = GP:foo=1]', [
+        new \Helmich\TypoScriptParser\Parser\AST\Operator\Assignment(
+            new \Helmich\TypoScriptParser\Parser\AST\ObjectPath('foo', 'foo'),
+            new \Helmich\TypoScriptParser\Parser\AST\Scalar('bar'),
+            2
+        ),
+    ], [], 1
+    ),
+    new \Helmich\TypoScriptParser\Parser\AST\ConditionalStatement(
+        '[globalVar = GP:foo=2]', [
+        new \Helmich\TypoScriptParser\Parser\AST\Operator\Assignment(
+            new \Helmich\TypoScriptParser\Parser\AST\ObjectPath('foo', 'foo'),
+            new \Helmich\TypoScriptParser\Parser\AST\Scalar('bar'),
+            6
+        ),
+    ], [], 5
+    ),
+];
